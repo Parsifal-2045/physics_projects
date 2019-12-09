fb58f6a1a1e87a2e55a9ee09e644ec3e0371c2e8
Parsifal-2045 <ferragina.luca@gmail.com>
2019-12-09 17:11:41 +0100

    Added excercise done in class, slightly optimised exceptions code

---
+++ code/algebra/calcolatrice/ex.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+
+/*class Error
+{
+    char n_op;
+    double n_l;
+    double n_r;
+
+public:
+    Error(char op, double l, double r) : n_op{op}, n_l{l}, n_r{r} {}
+    char op() const { return n_op; }
+    double l() const { return n_l; }
+    double r() const { return n_r; }
+};*/
+
+class DivideByZero{};
+struct InvalidOperator{char op;};
+
+double compute(char op, double const &l, double const &r)
+{
+    if (op == '+')
+    {
+        return l + r;
+    }
+    if (op == '-')
+    {
+        return l - r;
+    }
+    if (op == '*')
+    {
+        return l * r;
+    }
+    if (op == '/')
+    {
+        if (r == 0)
+        {
+            throw DivideByZero{};
+        }
+        else
+        {
+            return l / r;
+        }
+    }
+    else
+    {
+        throw InvalidOperator{op};
+    }
+}
+
+int main()
+{
+    try
+    {
+        compute('_', 1, 0);
+    }
+    catch (DivideByZero&)
+    {
+        std::cerr << "Error: cannot divide by 0" << '\n';
+    }
+    catch (InvalidOperator const &e)
+    {
+        std::cerr << "Error: invalid operand " << e.op << '\n';
+    }
+}
