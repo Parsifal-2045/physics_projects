8d30c71b421b10d18fd32fb2f50d50c93a5000dc
Parsifal-2045 <ferragina.luca@gmail.com>
2020-03-04 14:06:49 +0100

    Optimised is_prime code

--- code/functions/is_prime/is_prime.cpp
+++ code/functions/is_prime/is_prime.cpp
@@ -3,10 +3,19 @@
 
 bool is_prime(int const &n)
 {
-       int i = 2;
-    while (i < n)
+    int m;
+    int i = 2;
+    if (n < 0)
     {
-        if (n % i == 0)
+        m = -n;
+    }
+    else
+    {
+        m = n;
+    }
+    while (i < m)
+    {
+        if (m % i == 0)
         {
             return false;
         }
@@ -25,8 +34,6 @@ bool is_prime(int const &n)
     }
 }
 
-
-
 TEST_CASE("Testing is_prime")
 {
     CHECK(is_prime(1) == true);
@@ -34,6 +41,6 @@ TEST_CASE("Testing is_prime")
     CHECK(is_prime(11) == true);
     CHECK(is_prime(0) == false);
     CHECK(is_prime(2) == true);
-    CHECK(is_prime(7) == true);
-    CHECK(is_prime(9) == false);
+    CHECK(is_prime(-7) == true);
+    CHECK(is_prime(-9) == false);
 }
