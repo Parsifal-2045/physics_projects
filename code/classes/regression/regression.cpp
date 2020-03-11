2214136407425aefe773eb5376613aa12cc5eadb
Parsifal-2045 <ferragina.luca@gmail.com>
2020-03-11 10:58:25 +0100

    Added Regression class to calculate the parameters A and B of a linear fit

---
+++ code/classes/regression/regression.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+struct fit_s
+{
+    double A;
+    double B;
+};
+
+class Regression
+{
+private:
+    int m_N;
+    double m_sum_x;
+    double m_sum_y;
+    double m_sum_xy;
+    double m_sum_x2;
+
+public:
+    Regression() : m_N{0}, m_sum_x{0}, m_sum_y{0}, m_sum_xy{0}, m_sum_x2{0} {}
+    void add(double x, double y)
+    {
+        ++m_N;
+        m_sum_x += x;
+        m_sum_y += y;
+        m_sum_xy += x * y;
+        m_sum_x2 += x * x;
+    }
+
+    fit_s const fit()
+    {
+        double const num_A = (m_sum_y * m_sum_x2) - (m_sum_x * m_sum_xy);
+        double const num_B = (m_N * m_sum_xy) - (m_sum_x * m_sum_y);
+        double const den = (m_N * m_sum_x2) - (m_sum_x * m_sum_x);
+        double A = num_A / den;
+        double B = num_B / den;
+        fit_s result = {A,B};
+        return result;
+    }
+};
+
+int main()
+{
+    Regression reg;
+    reg.add(0, 0);
+    reg.add(1, 1);
+    auto result = reg.fit();
+    std::cout << "Y = " << result.A << " + " << result.B << " X\n";
+}
