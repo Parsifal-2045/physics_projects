398e4606c83fa993c6596b86d14d71643a844aa5
Parsifal-2045 <ferragina.luca@gmail.com>
2020-05-20 12:07:33 +0200

    Added file stream
    Number of people in different states is now saved on three files
    
    Those can be used as input files for ROOT to graph the epidemiological trend

--- code/exam/option_2/main.cpp
+++ code/exam/option_2/main.cpp
@@ -2,22 +2,49 @@
 #include <chrono>
 #include <random>
 #include <iostream>
+#include <fstream>
 #include "board.hpp"
 #include "display.hpp"
 
 int main()
 {
-    int const N = 150;
+    int const N = 45;
     Board test(N);
-    Display display{N};
-    display.draw(test);
     std::mt19937 gen(std::random_device{}());
     std::uniform_real_distribution<> infected(0., N);
     for (int i = 0; i != N / 10; ++i)
     {
         test(infected(gen), infected(gen)) = State::Infect;
     }
+    std::ofstream ofs;
+    std::ofstream ofs2;
+    std::ofstream ofs3;
+    ofs.open("/home/luca/root/programmazione/exam_option_2/dati_S.dat", std::ifstream::out);
+    ofs2.open("/home/luca/root/programmazione/exam_option_2/dati_I.dat", std::ifstream::out);
+    ofs3.open("/home/luca/root/programmazione/exam_option_2/dati_R.dat", std::ifstream::out);
+    if (ofs.is_open() && ofs2.is_open() && ofs3.is_open())
+    {
+        for (int i = 0; i != 100; ++i)
+        {
+            ofs << i << " " << test.GetSIR().S << '\n';
+            ofs2 << i << " " << test.GetSIR().I << '\n';
+            ofs3 << i << " " << test.GetSIR().R << '\n'; 
+            test.print();
+            test = evolve(test);
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
+        ofs.close();
+        ofs2.close();
+        ofs3.close();
+    }
+    else
+    {
+        std::cout << "Couldn't open the files" << '\n';
+    }
 
+    /*
+    Display display{N};
+    display.draw(test);
     std::cout << "Press any key to start the simulation. \n";
     display.WaitKeyPressed();
     for (int i = 0; i != 100; ++i)
@@ -28,19 +55,5 @@ int main()
     }
     std::cout << "Press any key to close the window. \n";
     display.WaitKeyPressed();
-
-    /*
-    std::mt19937 gen(std::random_device{}());
-    std::uniform_real_distribution<> infected(0., N);
-    for (int i = 0; i != N / 10; ++i)
-    {
-        test(infected(gen), infected(gen)) = State::Infect;
-    }
-    for (int i = 0; i != 100; ++i)
-    {
-        test.print();
-        test = evolve(test);
-        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    }
     */
 }
