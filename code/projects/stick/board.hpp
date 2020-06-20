a4638231fc14a5f7ef882346cf4ddd6d01f8f5bd
Parsifal-2045 <ferragina.luca@gmail.com>
2020-06-20 16:04:16 +0200

    Added slightly modified versions of the board and display classes

---
+++ code/projects/stick/board.hpp
@@ -0,0 +1,252 @@
+#ifndef BOARD_HPP
+#define BOARD_HPP
+
+#include <vector>
+#include <cassert>
+#include <iostream>
+#include <random>
+
+enum class State : char
+{
+    Susceptible,
+    Infect,
+    Recovered,
+    Dead
+};
+
+struct SIRD
+{
+    int S = 0;
+    int I = 0;
+    int R = 0;
+    int D = 0;
+};
+
+class Board
+{
+private:
+    int size_;
+    std::vector<State> data_;
+
+public:
+    Board(int n) : size_{n}, data_(n * n) {}
+
+    int size() const
+    {
+        return size_;
+    }
+
+    auto data() const
+    {
+        return data_;
+    }
+
+    State &operator()(int i, int j)
+    {
+        assert(i >= 0 && i < size_ && j >= 0 && j < size_);
+        return data_[i * size_ + j];
+    }
+
+    State GetCellState(int i, int j) const
+    {
+        if (i >= size_ || j >= size_ || i < 0 || j < 0)
+        {
+            return State::Recovered;
+        }
+        else
+        {
+            return data_[i * size_ + j];
+        }
+    }
+
+    auto CheckNeighbours(int i, int j) const
+    {
+        int infect = 0;
+        if (GetCellState(i, j) == State::Infect)
+        {
+            --infect;
+        }
+        if (j == 0)
+        {
+            for (int row = -1; row != 2; row++)
+            {
+                for (int column = 0; column != 2; column++)
+                {
+                    if (GetCellState(i + row, column) == State::Infect)
+                    {
+                        ++infect;
+                    }
+                }
+            }
+        }
+        if (j == size_ - 1)
+        {
+            for (int row = -1; row != 2; row++)
+            {
+                for (int column = -1; column != 1; column++)
+                {
+                    if (GetCellState(i + row, j + column) == State::Infect)
+                    {
+                        ++infect;
+                    }
+                }
+            }
+        }
+        if (j != 0 && j != size_ - 1)
+        {
+            for (int row = -1; row != 2; row++)
+            {
+                for (int column = -1; column != 2; column++)
+                {
+                    if (GetCellState(i + row, j + column) == State::Infect)
+                    {
+                        ++infect;
+                    }
+                }
+            }
+        }
+        assert(infect >= 0 && infect <= 8);
+        return infect;
+    }
+
+    SIRD GetSIRD()
+    {
+        int sus = 0;
+        int inf = 0;
+        int rec = 0;
+        int dead = 0;
+        for (int i = 0; i != size_; i++)
+        {
+            for (int j = 0; j != size_; j++)
+            {
+                if (GetCellState(i, j) == State::Susceptible)
+                {
+                    ++sus;
+                }
+                if (GetCellState(i, j) == State::Infect)
+                {
+                    ++inf;
+                }
+                if (GetCellState(i, j) == State::Recovered)
+                {
+                    ++rec;
+                }
+                if (GetCellState(i, j) == State::Dead)
+                {
+                    ++dead;
+                }
+            }
+        }
+        assert(sus + inf + rec + dead == static_cast<int>(data_.size()));
+        return SIRD{sus, inf, rec, dead};
+    }
+
+    void print()
+    {
+        std::cout << "\033c";
+        const std::string red("\033[0;31m");
+        const std::string green("\033[1;32m");
+        const std::string yellow("\033[1;33m");
+        const std::string cyan("\033[0;36m");
+        const std::string magenta("\033[0;35m");
+        const std::string reset("\033[0m");
+        for (int i = 0; i != size_; i++)
+        {
+            for (int j = 0; j != size_; j++)
+            {
+                auto status = static_cast<int>(data_[i * size_ + j]);
+                assert(status >= 0 && status <= 3);
+                if (status == 0)
+                {
+                    std::cout << "o ";
+                }
+                if (status == 1)
+                {
+                    std::cout << red << "i " << reset;
+                }
+                if (status == 2)
+                {
+                    std::cout << green << "o " << reset;
+                }
+                if (status == 3)
+                {
+                    std::cout << cyan << "x " << reset;
+                }
+            }
+            std::cout << '\n';
+        }
+    }
+};
+
+inline Board evolve(Board const &current)
+{
+    std::mt19937 gen(std::random_device{}());
+    std::uniform_real_distribution<> dis(0., 1.);
+    auto contagion_probability = dis(gen);
+    auto heal_probability = dis(gen);
+    auto death_probability = dis(gen);
+    int const N = current.size();
+    Board next(N);
+    for (int i = 0; i != N; ++i)
+    {
+        for (int j = 0; j != N; ++j)
+        {
+            std::mt19937 status_gen(std::random_device{}());
+            std::uniform_real_distribution<> status_dis(0., 1.);
+            auto status = status_dis(status_gen);
+            switch (current.GetCellState(i, j))
+            {
+            case State::Susceptible:
+                if (current.CheckNeighbours(i, j) == 0)
+                {
+                    next(i, j) = current.GetCellState(i, j);
+                    assert(next.GetCellState(i, j) == State::Susceptible);
+                }
+                else
+                {
+                    int infected = current.CheckNeighbours(i, j);
+                    auto contagion = infected * contagion_probability;
+                    if (contagion > status)
+                    {
+                        next(i, j) = State::Infect;
+                    }
+                    else
+                    {
+                        next(i, j) = current.GetCellState(i, j);
+                        assert(next.GetCellState(i, j) == State::Susceptible);
+                    }
+                }
+                break;
+
+            case State::Infect:
+                if (heal_probability >= status)
+                {
+                    next(i, j) = State::Recovered;
+                }
+                else if (death_probability >= status)
+                {
+                    next(i, j) = State::Dead;
+                }
+                else
+                {
+                    next(i, j) = current.GetCellState(i, j);
+                    assert(next.GetCellState(i, j) == State::Infect);
+                }
+                break;
+
+            case State::Recovered:
+                next(i, j) = current.GetCellState(i, j);
+                assert(next.GetCellState(i, j) == State::Recovered);
+                break;
+
+            case State::Dead:
+                next(i, j) = current.GetCellState(i, j);
+                assert(next.GetCellState(i, j) == State::Dead);
+                break;
+            }
+        }
+    }
+    return next;
+}
+
+#endif
---
+++ code/projects/stick/display.hpp
@@ -0,0 +1,96 @@
+#ifndef BOARD_DISPLAY_HPP
+#define BOARD_DISPLAY_HPP
+
+#include "board.hpp"
+#include <cassert>
+#include <SFML/Graphics.hpp>
+
+class Display
+{
+private:
+    int board_side_;
+    sf::RenderWindow window_;
+    static constexpr int cell_size_ = 5;
+    static int display_side(int board_side)
+    {
+        return board_side * cell_size_;
+    }
+    static constexpr auto window_title = "Evolution of an epidemic";
+
+public:
+    Display(int board_side) : board_side_{board_side},
+                              window_{sf::VideoMode(display_side(board_side), display_side(board_side)), window_title, sf::Style::Close}
+    {
+    }
+    void draw(Board const &board)
+    {
+        assert(board_side_ == board.size());
+        window_.clear(sf::Color::White);
+
+        sf::RectangleShape infected(sf::Vector2f(cell_size_, cell_size_));
+        infected.setFillColor(sf::Color::Red);
+        infected.setOutlineThickness(1.f);
+        infected.setOutlineColor(sf::Color::White);
+
+        sf::RectangleShape recovered(sf::Vector2f(cell_size_, cell_size_));
+        recovered.setFillColor(sf::Color::Green);
+        recovered.setOutlineThickness(1.f);
+        recovered.setOutlineColor(sf::Color::White);
+
+        sf::RectangleShape dead(sf::Vector2f(cell_size_, cell_size_));
+        dead.setFillColor(sf::Color::Black);
+        dead.setOutlineThickness(1.f);
+        dead.setOutlineColor(sf::Color::White);
+
+        int const N = board.size();
+
+        for (int i = 0; i != N; ++i)
+        {
+            for (int j = 0; j != N; ++j)
+            {
+                if (board.GetCellState(i, j) == State::Infect)
+                {
+                    infected.setPosition(j * cell_size_, i * cell_size_);
+                    window_.draw(infected);
+                }
+                if (board.GetCellState(i, j) == State::Recovered)
+                {
+                    recovered.setPosition(j * cell_size_, i * cell_size_);
+                    window_.draw(recovered);
+                }
+                if (board.GetCellState(i, j) == State::Dead)
+                {
+                    dead.setPosition(j * cell_size_, i * cell_size_);
+                    window_.draw(dead);
+                }
+            }
+        }
+        window_.display();
+    }
+    bool WaitKeyPressed()
+    {
+        sf::Event event;
+        bool action;
+        while (window_.waitEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window_.close();
+                action = false;
+                break;
+            }
+            if (event.type == sf::Event::KeyPressed)
+            {
+                action = true;
+                break;
+            }
+            if ((event.type != sf::Event::Closed) && (event.type != sf::Event::KeyPressed))
+            {
+                continue;
+            }
+        }
+        return action;
+    }
+};
+
+#endif
