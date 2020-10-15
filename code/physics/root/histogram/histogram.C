288fb7e07cf4f5e7b3c2f59ed0c05a7cd2a408be
Parsifal-2045 <ferragina.luca@gmail.com>
2020-10-15 14:47:01 +0200

    Added macro to produce a simple histogram

---
+++ code/physics/root/histogram/histogram.C
@@ -0,0 +1,33 @@
+void setStyle()
+{
+    gROOT->SetStyle("Plain");
+    gStyle->SetPalette(57);
+    gStyle->SetOptTitle(0);
+}
+
+void Histo(int ngen = 10E6)
+{
+    TH1F *h = new TH1F("h", "Test histogram", 100, -5., 5.);
+
+    h->SetFillColor(kBlue);
+    h->SetLineColor(kBlack);
+    h->GetXaxis()->SetTitle("x");
+    h->GetYaxis()->SetTitle("entries");
+
+    h->FillRandom("gaus", ngen);
+
+    TCanvas *c1 = new TCanvas("c1");
+    
+    h->Draw("H");
+    h->Draw("E,P,SAME");
+
+    double hEntries = h->GetEntries();
+    double hMean = h->GetMean();
+    double hRMS = h->GetRMS();
+    double hMeanError = h->GetMeanError();
+    double hRMSError = h->GetRMSError();
+
+    std::cout << "Entries: " << hEntries << '\n';
+    std::cout << "Mean: " << hMean << " +/- " << hMeanError << '\n';
+    std::cout << "RMS: " << hRMS << " +/- " << hRMSError << '\n';
+}
