409452b85862070a4c96171ce61496953ab9c695
Parsifal-2045 <ferragina.luca@gmail.com>
2020-10-22 11:24:33 +0200

    Updated main to test the new Particle class

--- code/physics/root/particle/main.cpp
+++ code/physics/root/particle/main.cpp
@@ -1,10 +1,40 @@
+#include <iostream>
+#include <vector>
 #include "particle_type.hpp"
 #include "resonance_type.hpp"
+#include "particle.hpp"
 
 int main()
 {
-    ParticleType a{"Proton", 12.6, 1};
-    a.print();
-    ResonanceType b{"test 1", 25, 4, 1};
-    b.print();
+    Particle::AddParticleType("Test 0", 0, 0, 0);
+    Particle::AddParticleType("Test 1", 1, 1, 1);
+    Particle::AddParticleType("Test 2", 2, 2, 2);
+    Particle::AddParticleType("Test 3", 3, 3, 3);
+    Particle::AddParticleType("Test 4", 4, 4, 4);
+    Particle::AddParticleType("Test 5", 5, 5, 5);
+    Particle::AddParticleType("Test 6", 6, 6, 6);
+    Particle::AddParticleType("Test 7", 7, 7, 7);
+    Particle::AddParticleType("Test 8", 8, 8, 8);
+    Particle::AddParticleType("Test 9", 9, 9, 9);
+    Particle::AddParticleType("Test 10", 10, 10, 10);
+    Particle p{"Test 0", 0, 0, 0};
+    p.PrintParticle();
+    p.SetP(2, 2, 2);
+    p.PrintParticle();
 }
+
+/*  Set of 10 Particles for testing
+
+    Particle::AddParticleType("Test 0", 0, 0, 0);
+    Particle::AddParticleType("Test 1", 1, 1, 1);
+    Particle::AddParticleType("Test 2", 2, 2, 2);
+    Particle::AddParticleType("Test 3", 3, 3, 3);
+    Particle::AddParticleType("Test 4", 4, 4, 4);
+    Particle::AddParticleType("Test 5", 5, 5, 5);
+    Particle::AddParticleType("Test 6", 6, 6, 6);
+    Particle::AddParticleType("Test 7", 7, 7, 7);
+    Particle::AddParticleType("Test 8", 8, 8, 8);
+    Particle::AddParticleType("Test 9", 9, 9, 9);
+    Particle::AddParticleType("Test 10", 10, 10, 10);
+
+*/
