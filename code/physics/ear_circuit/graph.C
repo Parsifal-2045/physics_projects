f6a6a6ed20f9eafaf989581adf3960b2edd07580
Parsifal-2045 <ferragina.luca@gmail.com>
2021-05-21 19:32:14 +0200

    Added ROOT macro to produce two graphs
    The graphs will be used in a lab report

---
+++ code/physics/ear_circuit/graph.C
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <TCanvas.h>
+#include <TGraph.h>
+#include <TMath.h>
+
+void graph()
+{
+    TCanvas *c_3 = new TCanvas("c_3", "Risposta in frequenza primo circuito");
+    c_3->SetGrid();
+    TGraph *g_3 = new TGraph("V_R_3.dat", "%lg %lg");
+    g_3->SetTitle("Risposta in frequenza del circuito risonante a frequenza minore");
+    g_3->GetXaxis()->SetTitle("Frequenza (Hz)");
+    g_3->GetYaxis()->SetTitle("Ampiezza (V)");
+
+    auto y_3 = g_3->GetY();
+    auto x_3 = g_3->GetX();
+    int n_3 = g_3->GetN();
+    int i_3 = std::max_element(y_3, y_3 + n_3) - y_3;
+    std::cout << "Frequenza di risonanza primo circuito = " << x_3[i_3] << '\n';
+
+    g_3->Draw();
+    c_3->Print("primo_circuito.pdf");
+
+    TCanvas *c_9 = new TCanvas("c_9", "Risposta in frequenza secondo circuito");
+    c_9->SetGrid();
+    TGraph *g_9 = new TGraph("V_R_9.dat", "%lg %lg");
+    g_9->SetTitle("Risposta in frequenza del circuito risonante a frequenza maggiore");
+    g_9->GetXaxis()->SetTitle("Frequenza (Hz)");
+    g_9->GetYaxis()->SetTitle("Ampiezza (V)");
+
+    auto y_9 = g_9->GetY();
+    auto x_9 = g_9->GetX();
+    int n_9 = g_9->GetN();
+    int i_9 = std::max_element(y_9, y_9 + n_9) - y_9;
+    std::cout << "Frequenza di risonanza secondo circuito = " << x_9[i_9] << '\n';
+
+    g_9->Draw();
+    c_9->Print("secondo_circuito.pdf");
+
+    TCanvas *c_sum = new TCanvas("c_sum", "Somma delle risposte in frequenza");
+    c_sum->SetGrid();
+    TGraph *g_sum = new TGraph("V_R_tot.dat", "%lg %lg");
+    g_sum->SetTitle("Risposta in frequenza");
+    g_sum->GetXaxis()->SetTitle("Frequenza (Hz)");
+    g_sum->GetYaxis()->SetTitle("Ampiezza (V)");
+
+    g_sum->Draw();
+    c_sum->Print("somma_maglie.pdf");
+}
