9a4f7234185d5f97edbc6d473fdcfd018f658761
Parsifal-2045 <ferragina.luca@gmail.com>
2023-12-18 10:56:33 +0100

    Implemented seeding and its monitoring

--- physics/track_reco/main-template-start.cxx
+++ physics/track_reco/main-template-start.cxx
@@ -27,6 +27,13 @@ void generate(int nTrk = 10)
   hBNum->Sumw2();
   hBDen->Sumw2();
 
+  // graphs/function for seeding part
+  TGraphErrors *graph = new TGraphErrors();         // used in seeding used to fit the candidate tracks and get their chi2
+  TF1 *line = new TF1("line", "[0]+[1]*x", 0, 150); // linear function for track formation in seeding step
+
+  TH1F *hFit = new TH1F("hFit", " Seed Fit quality", 1000, 0, 100);              // chi2 of all seeds
+  TH1F *hFitGood = new TH1F("hFitGood", " Good Seed Fit quality", 1000, 0, 100); // chi2 of seeds that come from a single generated track (check that all labels of the track's points are the same)
+
   const Int_t nEvt = 100; // generate nEvt events
 
   // Detector dimensions
@@ -44,6 +51,11 @@ void generate(int nTrk = 10)
 
   Double_t A = 0, B = 0;
   Double_t xImpact, yImpact;
+
+  // seeding parameters
+  Double_t maxChi = 100.;
+  int nTotSeed = 0;
+  int nTotGoodSeed = 0;
 
   // start loop over events
   for (Int_t ievt = 0; ievt < nEvt; ievt++)
@@ -94,8 +106,81 @@ void generate(int nTrk = 10)
       }
       hBDen->Fill(B);
     } // close loop on generated tracks
-  } // end loop over the events
 
+    //_____________________________________________
+    // Seeding
+    //_____________________________________________
+
+    Int_t nSeed = 0;
+    recoTrack Track[1000]; // allow for a larger size (fake seeds)
+
+    // add a first constraint: the vertex
+    graph->SetPoint(0, 0, 0);
+    graph->SetPointError(0, 0.1, 2); // nominal vertex, 2cm beam-spot size
+    // loop on the three last external planes (seeding from outside)
+    for (Int_t i1 = 0; i1 < nHitsP[nPlanes - 1]; i1++)
+    {
+      for (Int_t i2 = 0; i2 < nHitsP[nPlanes - 2]; i2++)
+      {
+        for (Int_t i3 = 0; i3 < nHitsP[nPlanes - 3]; i3++)
+        {
+          graph->SetPoint(1, Hits[nPlanes - 1][i1].GetX(), Hits[nPlanes - 1][i1].GetY());
+          graph->SetPoint(2, Hits[nPlanes - 2][i2].GetX(), Hits[nPlanes - 2][i2].GetY());
+          graph->SetPoint(3, Hits[nPlanes - 3][i3].GetX(), Hits[nPlanes - 3][i3].GetY());
+          graph->SetPointError(1, 0.1, 0.2); // uncertainty on detector planes "no width" and 2mm along y
+          graph->SetPointError(2, 0.1, 0.2);
+          graph->SetPointError(3, 0.1, 0.2);
+          bool status = graph->Fit(line, "Q");               // status is 0 if the fit converges
+          hFit->Fill(line->GetChisquare() / line->GetNDF()); // fit quality for all seeds
+          graph->Clear();
+          if (!status && (line->GetChisquare() / line->GetNDF() < maxChi)) // fit converged and the chi2 is less than the limit set before
+          {                                                                // take this as a good seed (X**2/NDF<maxChi)
+            Track[nSeed].SetA(line->GetParameter(0));                      // set parameters of the reconstucted track
+            Track[nSeed].SetB(line->GetParameter(1));
+            Track[nSeed].SetdA(line->GetParError(0));
+            Track[nSeed].SetdB(line->GetParError(1));
+            Track[nSeed].SetchiSeed(line->GetChisquare() / line->GetNDF()); // set chi2 of the reconstructed track
+            Track[nSeed].AddHit(Hits[nPlanes - 1][i1].GetTrackIndex());     // add hits to the reconstructed track
+            Track[nSeed].AddHit(Hits[nPlanes - 2][i2].GetTrackIndex());
+            Track[nSeed].AddHit(Hits[nPlanes - 3][i3].GetTrackIndex());
+            nSeed++; // move to the next seed
+          }
+        }
+      }
+    }
+
+    // Now check how many seeds have the right hit assignment
+    bool badSeed = false;
+    int nGoodSeed = 0;
+    int *labels = new int[nPlanes];
+    for (Int_t igenTrk = 0; igenTrk < nTrk; igenTrk++)
+    { // loop on gen tracks
+      for (Int_t iTrk = 0; iTrk < nSeed; iTrk++)
+      { // loop on seeds
+        badSeed = false;
+        labels = Track[iTrk].GetHits();
+        for (Int_t i = 0; i < Track[iTrk].GetNHits(); i++)
+        {
+          if (igenTrk != labels[i])
+          {
+            badSeed = true; // 1 wrong label makes the seed bad
+          }
+        }
+        if (!badSeed)
+        {
+          nGoodSeed++;                              // if all track labels are that of the track, good seed found
+          hFitGood->Fill(Track[iTrk].GetchiSeed()); // fit quality for good seeds
+          break;
+        }
+      }
+    }
+
+    nTotSeed += nSeed;
+    nTotGoodSeed += nGoodSeed;
+
+  } // close loop over the events
+
+  // monitor acceptance
   TH1F *hAcc = new TH1F(*hBDen);
   hAcc->SetTitle("Acceptance vs B, at least 8 hits");
   hAcc->Divide(hBNum, hBDen, 1, 1, "B"); // acceptance = # tracks with at least 8 hits / total # of generated tracks
@@ -109,5 +194,17 @@ void generate(int nTrk = 10)
   cPlot1->cd(3);
   hAcc->Draw();
 
-  return;
+  // monitor seeding
+  std::cout << "Total number of seeds = " << nTotSeed << std::endl;
+  std::cout << "Total number of good seeds = " << nTotGoodSeed << std::endl;
+  std::cout << "Seeding efficiency = " << float(nTotGoodSeed) / nTrk / nEvt << std::endl; // number of good seeds / (number of tracks per event * number of events)
+  std::cout << "Seeding purity = " << float(nTotGoodSeed) / nTotSeed << std::endl;        // number of good seeds / total number of seeds
+  // 100 is way too large as a cut on chi2 -> leads to low purity
+  // check cuts around chi2 = 10 with seeds-qual and settle on a better cut
+  // selected chi2 cut at 4 in the end
+
+  TCanvas *cPlot2 = new TCanvas();
+  hFit->Draw();
+  hFitGood->SetFillColor(kBlue);
+  hFitGood->Draw("same");
 }
