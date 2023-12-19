6f21ea5aaf02f77b60cdb16918b0ed2417bdfded
Parsifal-2045 <ferragina.luca@gmail.com>
2023-12-19 16:14:26 +0100

    Implemented Kalman filtering for track propagation

--- physics/track_reco/recoTrack.cxx
+++ physics/track_reco/recoTrack.cxx
@@ -1,14 +1,26 @@
 #include <iostream>
 
-#include "TMath.h"
 #include "recoTrack.h"
+#include "TMath.h"
 
-recoTrack::~recoTrack() { delete[] fHits; }
 recoTrack::recoTrack()
 {
     fNHits = 0;
     fHits = new int[10];
+    fIsGoodSeed = false;
+    fIsGoodReco = false;
+    fIsFullReco = false;
+    fCov = new TMatrix(2, 2);
+    fX = new TMatrix(2, 1);
 }
+
+recoTrack::~recoTrack()
+{
+    delete[] fHits;
+    delete fCov;
+    delete fX;
+}
+
 void recoTrack::AddHit(int index)
 {
     if (fNHits < 10)
@@ -18,23 +30,134 @@ void recoTrack::AddHit(int index)
     }
     else
     {
-        std::cout << " could not add the hit" << std::endl;
+        std::cout << " Could not add the hit" << std::endl;
     }
 }
 void recoTrack::PrintLabels() const
 {
     for (Int_t i = 0; i < fNHits; i++)
-        std::cout << "track label = [ " << i << "] is:" << fHits[i] << std::endl;
+        std::cout << "Track label = [ " << i << "] is:" << fHits[i] << std::endl;
 }
 double recoTrack::GetA() const { return fA; }
 double recoTrack::GetB() const { return fB; }
 double recoTrack::GetdA() const { return fdA; }
 double recoTrack::GetdB() const { return fdB; }
+double recoTrack::GetStateA() const { return (*fX)(0, 0); }
+double recoTrack::GetStateB() const { return (*fX)(1, 0); }
+double recoTrack::GetStatedA() const { return TMath::Sqrt(TMath::Abs((*fCov)(0, 0))); }
+double recoTrack::GetStatedB() const { return TMath::Sqrt(TMath::Abs((*fCov)(1, 1))); }
 double recoTrack::GetchiSeed() const { return fchiSeed; }
+
 double recoTrack::GetNHits() const { return fNHits; }
+bool recoTrack::IsGoodSeed() const { return fIsGoodSeed; }
+bool recoTrack::IsGoodReco() const { return fIsGoodReco; }
+bool recoTrack::IsFullReco() const { return fIsFullReco; }
 int *recoTrack::GetHits() const { return fHits; }
 void recoTrack::SetA(double A) { fA = A; }
 void recoTrack::SetB(double B) { fB = B; }
 void recoTrack::SetdA(double dA) { fdA = dA; }
 void recoTrack::SetdB(double dB) { fdB = dB; }
 void recoTrack::SetchiSeed(double chiSeed) { fchiSeed = chiSeed; }
+void recoTrack::SetGoodSeed() { fIsGoodSeed = true; }
+void recoTrack::SetFullReco() { fIsFullReco = true; }
+void recoTrack::SetGoodReco() { fIsGoodReco = true; }
+void recoTrack::InitCovariance(TMatrix &cov) { *fCov = cov; }
+void recoTrack::InitStateVector(TMatrix &x) { *fX = x; }
+void recoTrack::InitStatex(double xState) { fxCoord = xState; }
+void recoTrack::PrintCovariance() const { fCov->Print(); }
+void recoTrack::PrintStateVector() const { fX->Print(); }
+void recoTrack::PrintStatex() const { std::cout << "State was propagated at x = " << fxCoord << std::endl; }
+
+void recoTrack::PropagateAndFilter(double xProp, double yMeas, bool print)
+{
+
+    // Kalman Filter (Prediction step)
+    TMatrix F(2, 2);
+    F(0, 0) = 1;
+    F(0, 1) = xProp - fxCoord;
+    F(1, 0) = 0;
+    F(1, 1) = 1; // for System equation, F matrix
+    TMatrix H(1, 2);
+    H(0, 0) = 1;
+    H(0, 1) = 0; // for Measurement equation, H matrix
+    TMatrix FT(2, 2);
+    FT.Transpose(F);
+    TMatrix HT(2, 1);
+    HT.Transpose(H);
+
+    TMatrix xk(2, 1);
+    TMatrix xk_prop(2, 1);
+    TMatrix xk_filter(2, 1); // state vector, redicted state and filtered state
+    TMatrix Ck(2, 2);
+    TMatrix Ck_prop(2, 2);
+    TMatrix Ck_prop_inv(2, 2); // correspondig covariance
+    TMatrix Ck_filter(2, 2);
+    TMatrix Ck_filter_inv(2, 2);
+
+    double invV = 0;
+    if (TMath::Abs(yMeas) > 0)
+    {
+        invV = 1. / 0.2 / 0.2; // if a measurement is available define measurement covariance matrix: 1 / sigma^2
+    }
+    double M = yMeas; // the measurement
+
+    TMatrix HTinvVm(2, 1);
+    HTinvVm(0, 0) = M * invV;
+    HTinvVm(1, 0) = 0;
+    TMatrix HTinvVH(2, 2);
+    HTinvVH(0, 0) = invV;
+    HTinvVH(0, 1) = 0;
+    HTinvVH(1, 0) = 0;
+    HTinvVH(1, 1) = 0;
+
+    xk = *fX;   // current state
+    Ck = *fCov; // input from the internal covariance
+    if (print)
+    {
+        std::cout << "fxCoord =" << fxCoord << std::endl;
+        std::cout << "Measurement = " << yMeas << std::endl;
+        std::cout << "Initial state vector and covariance " << std::endl;
+        xk.Print();
+        Ck.Print();
+    }
+
+    // propagation
+    xk_prop = F * xk;      // predicted state
+    Ck_prop = F * Ck * FT; // predicted covariance
+
+    if (print)
+    {
+        std::cout << "Predicted state vector and covariance " << std::endl;
+        xk_prop.Print();
+        Ck_prop.Print();
+    }
+
+    // filter
+    Ck_prop_inv = Ck_prop;
+    Ck_prop_inv.Invert();
+
+    // filtered state
+    xk_filter = (Ck_prop_inv * xk_prop + HTinvVm); // numerator
+    Ck_filter_inv = (Ck_prop_inv + HTinvVH);       // denominator
+    Ck_filter = Ck_filter_inv.Invert();
+    xk_filter = Ck_filter * xk_filter;
+
+    if (print)
+    {
+        std::cout << "Filtered state vector and covariance " << std::endl;
+        xk_filter.Print();
+        Ck_filter.Print();
+    }
+
+    // update the internal state
+    *fX = xk_filter;
+    // update internal Covariance
+    *fCov = Ck_filter;
+    // update x propagation coordinate of the state
+    fxCoord = xProp;
+
+    if (print)
+        std::cout << " Final fxCoord =" << fxCoord << std::endl;
+
+    return;
+}
--- physics/track_reco/recoTrack.h
+++ physics/track_reco/recoTrack.h
@@ -1,5 +1,6 @@
 #ifndef RECOTRACK_H
 #define RECOTRACK_H
+#include "TMatrix.h"
 
 class recoTrack
 {
@@ -11,8 +12,15 @@ public:
     double GetB() const;
     double GetdA() const;
     double GetdB() const;
+    double GetStateA() const;
+    double GetStateB() const;
+    double GetStatedA() const;
+    double GetStatedB() const;
     double GetchiSeed() const;
     double GetNHits() const;
+    bool IsGoodSeed() const; // seed formed with 3 points from the outer layers all with the same label
+    bool IsFullReco() const; // all 10 hits share the same label
+    bool IsGoodReco() const; // passes the quality check (usually more than 80% of the hits share the same label)
     int *GetHits() const;
     void PrintLabels() const;
     void SetA(double A);
@@ -20,11 +28,27 @@ public:
     void SetdA(double dA);
     void SetdB(double dB);
     void SetchiSeed(double chiSeed);
+    void InitCovariance(TMatrix &cov);
+    void InitStateVector(TMatrix &x);
+    void InitStatex(double xState);
+    void PropagateAndFilter(double xProp, double yMeas = 0, bool print = false);
+    void PrintCovariance() const;
+    void PrintStateVector() const;
+    void PrintStatex() const;
+    void SetGoodSeed();
+    void SetFullReco();
+    void SetGoodReco();
 
 private:
-    double fA, fB, fdA, fdB; // the reco track parameters: A -> intercept, B -> slope
+    double fA, fB, fdA, fdB; // the reco track parameter
     double fchiSeed;         // the Chi**2/NDF of the seed
-    int *fHits;              // pointer to the array of hits associated to the track, stores the labels of the true track that generated the hit (max size 10 as the detector planes)
-    int fNHits;              // the number of hits associated to the track. Expected 10 if all hits are in the acceptance region of the detector, could be < 10. Can be used to define acceptance
+    TMatrix *fCov;           // the state covariance
+    TMatrix *fX;             // The state vector
+    double fxCoord;          // the x coordinate of the state
+    int *fHits;              // pointer to the array of hits gen labels (max size 10 as the detector planes)
+    int fNHits;              // the number of hits in the track
+    Bool_t fIsGoodSeed;
+    Bool_t fIsFullReco;
+    Bool_t fIsGoodReco;
 };
 #endif
